e292a3e1ca65e67771cd8d5ff361c18731e25518
alextanhongpin <alextan220990@gmail.com>
2017-09-09 10:43:41 +0800

    fix: improve write-json.go with suggestion

--- write-json.go
+++ write-json.go
@@ -4,8 +4,10 @@ package main
 
 import (
 	"encoding/json"
-	"io/ioutil"
+	"encoding/xml"
+	"io"
 	"log"
+	"os"
 )
 
 // Point represents the schema of our json output
@@ -14,7 +16,20 @@ type Point struct {
 	Y int `json:"y"`
 }
 
-func writeJSON(file string, obj interface{}, pretty bool) (err error) {
+// func writeJSONAlt(file string, obj interface{}, pretty bool) (err error) {
+// 	var bytes []byte
+// 	if pretty {
+// 		bytes, err = json.MarshalIndent(obj, "", "  ")
+// 	} else {
+// 		bytes, err = json.Marshal(obj)
+// 	}
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return ioutil.WriteFile(file, bytes, 0644)
+// }
+
+func writeJSON(w io.WriteCloser, obj interface{}, pretty bool) (err error) {
 	var bytes []byte
 	if pretty {
 		bytes, err = json.MarshalIndent(obj, "", "  ")
@@ -24,13 +39,39 @@ func writeJSON(file string, obj interface{}, pretty bool) (err error) {
 	if err != nil {
 		return err
 	}
-	return ioutil.WriteFile(file, bytes, 0644)
+	w.Write(bytes)
+	return
+}
+
+func writeXML(w io.WriteCloser, obj interface{}, pretty bool) (err error) {
+	var bytes []byte
+	if pretty {
+		bytes, err = xml.MarshalIndent(obj, "", "  ")
+	} else {
+		bytes, err = xml.Marshal(obj)
+	}
+	if err != nil {
+		return err
+	}
+	w.Write(bytes)
+	return
 }
 
 func main() {
 	points := []Point{Point{0, 0}, Point{1, 1}}
-	err := writeJSON("out.json", points, true)
-	if err != nil {
-		log.Fatalf("error writing to json: %v\n", err)
+	var w io.WriteCloser
+	var err error
+
+	runningInPlayGround := os.Getenv("user") == ""
+	if !runningInPlayGround {
+		w, err = os.Create("myfile.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		w = os.Stdout
 	}
+	writeJSON(w, points, true)
+	writeXML(w, points, true)
+	w.Close()
 }
